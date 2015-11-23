da24c8e7257ec26675830ebf9f8117c0bf001488
achaabni <achaabni@jahia.com>
2015-11-23 17:28:28 +0100

    BACKLOG-5738 : Initial JCR structure creation (Adding path existence verification tests)

--- test/src/main/java/org/jahia/test/services/modulemanager/BundleServiceTest.java
+++ test/src/main/java/org/jahia/test/services/modulemanager/BundleServiceTest.java
@@ -1,19 +1,78 @@
 package org.jahia.test.services.modulemanager;
 
-import org.jahia.modules.modulemanager.impl.BundleServiceImpl;
+import org.jahia.services.content.JCRNodeIteratorWrapper;
+import org.jahia.services.content.JCRNodeWrapper;
+import org.jahia.services.content.JCRSessionWrapper;
+import org.jahia.services.content.JCRTemplate;
 import org.jahia.test.JahiaTestCase;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import javax.jcr.PathNotFoundException;
+import javax.jcr.RepositoryException;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by achaabni on 20/11/15.
  */
-public class BundleServiceTest extends JahiaTestCase{
+public class BundleServiceTest extends JahiaTestCase {
+
+    private static JCRSessionWrapper session;
+
+    @BeforeClass
+    public static void oneTimeSetUp() throws Exception {
+        session = JCRTemplate.getInstance().getSessionFactory().getCurrentSystemSession("default", null, null);
+    }
+
 
     @Test
-    public void testService(){
-        System.out.print("aaaa");
-        assertEquals(1,1);
+    public void testModuleManagementNodePresence() throws RepositoryException {
+        assertNotNull(session.getNode("/module-management"));
+    }
+
+    @Test
+    public void testBundlesNodePresence() throws RepositoryException {
+        assertNotNull(session.getNode("/module-management/bundles"));
+    }
+
+    @Test
+    public void testClusterNodesNodePresence() throws RepositoryException {
+        assertNotNull(session.getNode("/module-management/nodes"));
+    }
+
+    @Test
+    public void testOperationsNodePresence() throws RepositoryException {
+        assertNotNull(session.getNode("/module-management/operations"));
+    }
+
+    @Test
+    public void testOperationLogNodePresence() throws RepositoryException {
+        assertNotNull(session.getNode("/module-management/operationLog"));
+    }
+
+
+    @Test
+    public void testBundleChildrenNotEmpty() throws RepositoryException {
+        assertTrue(session.getNode("/module-management/bundles").hasNodes());
+    }
+
+    @Test
+    public void testClusterNodesChildrenNotEmpty() throws RepositoryException {
+        assertTrue(session.getNode("/module-management/nodes").hasNodes());
+    }
+
+    @Test
+    public void testEqualBundleSize() throws RepositoryException {
+
+            long bundlesSize = session.getNode("/module-management/bundles").getNodes().getSize();
+            JCRNodeIteratorWrapper nodes = session.getNode("/module-management/nodes").getNodes();
+            while (nodes.hasNext()) {
+                String clusterNodePath = ((JCRNodeWrapper) nodes.next()).getPath();
+                JCRNodeIteratorWrapper clusterBundleNodes = session.getNode(clusterNodePath + "/bundles").getNodes();
+                assertEquals(bundlesSize, clusterBundleNodes.getSize());
+            }
     }
 }
