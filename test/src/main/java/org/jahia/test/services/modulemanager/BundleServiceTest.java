e3b06e6a899188c0b0df6746c92089a7372ef922
Sergiy Shyrkov <sshyrkov@jahia.com>
2015-11-23 12:34:38 +0100

    Finished splitting module into core and test

---
+++ test/src/main/java/org/jahia/test/services/modulemanager/BundleServiceTest.java
@@ -0,0 +1,19 @@
+package org.jahia.test.services.modulemanager;
+
+import org.jahia.modules.modulemanager.impl.BundleServiceImpl;
+import org.jahia.test.JahiaTestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by achaabni on 20/11/15.
+ */
+public class BundleServiceTest extends JahiaTestCase{
+
+    @Test
+    public void testService(){
+        System.out.print("aaaa");
+        assertEquals(1,1);
+    }
+}
