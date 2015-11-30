7e933b239c671aa82d1a8dd455831a2c46a38d97
achaabni <achaabni@jahia.com>
2015-11-30 15:04:20 +0100

    BACKLOG-6043 : REST - command line scripts for module management (Remove wait for transactions method content  - to be added after adding the service

--- core/src/main/bin/common.sh
+++ core/src/main/bin/common.sh
@@ -8,20 +8,4 @@ WAIT_TRIES=30
 CURL_OPTIONS="-s --user $DF_USERNAME:$DF_PASSWORD"
 
 function waitForTransactions() {
-TRANSACTIONS_OUTPUT=
-TRIES=0
-echo -n "Waiting for transaction to end."
-while [[ $TRANSACTIONS_OUTPUT != *",\"open\":false"* ]] && [ $TRIES -lt $WAIT_TRIES ]; do
-    #now let's retrieve the transaction list
-    TRANSACTIONS_OUTPUT=`curl $CURL_OPTIONS $DF_SERVER_BASE_URL/modules/api/cluster/v1/transaction`
-    # echo $TRANSACTIONS_OUTPUT tries=$TRIES
-    echo -n "."
-    sleep $WAIT_INTERVAL
-    let TRIES=TRIES+1
-done
-if [ "$TRIES" -eq "$WAIT_TRIES" ]; then
-echo Reached maximum number of wait tries, transaction did not complete successfully after elapsed time!
-else
-echo Transaction completed successfully.
-fi
 }
