6f2baf7d2a2f9b5938bdeda8f22bc4d9649836b5
achaabni <achaabni@jahia.com>
2015-12-02 15:23:33 +0100

    BACKLOG-6043 : REST - command line scripts for module management

--- core/src/main/bin/getBundleState.sh
+++ core/src/main/bin/getBundleState.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-# example script to start a bundle using the module manager's REST API
+# example script to get a bundle state on a list of nodes using the module manager's REST API
 source ./common.sh
-#start bundle REST call
+#get bundle state REST call
 echo Getting state for  bundle $1...
 STATE_OUTPUT=`curl $CURL_OPTIONS --request GET $DX_SERVER_BASE_URL/$DX_MM_REST_PREFIX/$1/_state`
-echo "Start result=$STATE_OUTPUT"
+echo "State result=$STATE_OUTPUT"
