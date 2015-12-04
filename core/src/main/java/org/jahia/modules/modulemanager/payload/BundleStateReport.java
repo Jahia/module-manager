0ef0f8abdc622967a6c8fd6282d4df0b4a549b45
Sergiy Shyrkov <sshyrkov@jahia.com>
2015-12-04 11:28:39 +0100

    Cosmetics: renamed fields ( BACKLOG-6051 )

--- core/src/main/java/org/jahia/modules/modulemanager/payload/BundleStateReport.java
+++ core/src/main/java/org/jahia/modules/modulemanager/payload/BundleStateReport.java
@@ -3,10 +3,13 @@ package org.jahia.modules.modulemanager.payload;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Map;
 
+/**
+ * Represents a bundle state.
+ */
 @XmlRootElement
 public class BundleStateReport {
 
-    private String clusterBundle;
+    private String bundle;
     
     private Map<String, String> nodeStates;
 
@@ -15,16 +18,16 @@ public class BundleStateReport {
     }
 
     public BundleStateReport(String clusterBundle, Map<String, String> nodeStates) {
-        this.clusterBundle = clusterBundle;
+        this.bundle = clusterBundle;
         this.nodeStates = nodeStates;
     }
 
-    public String getClusterBundle() {
-        return clusterBundle;
+    public String getBundle() {
+        return bundle;
     }
 
-    public void setClusterBundle(String clusterBundle) {
-        this.clusterBundle = clusterBundle;
+    public void setBundle(String clusterBundle) {
+        this.bundle = clusterBundle;
     }
 
     public Map<String, String> getNodeStates() {
--- core/src/main/java/org/jahia/modules/modulemanager/payload/NodeStateReport.java
+++ core/src/main/java/org/jahia/modules/modulemanager/payload/NodeStateReport.java
@@ -3,16 +3,18 @@ package org.jahia.modules.modulemanager.payload;
 import java.util.Set;
 
 /**
+ * Represents an information about bundle states of the current DX node.
+ * 
  * @author achaabni
  */
 public class NodeStateReport {
 
     private String nodeId;
-    private Set<BundleStateReport> bundleStateReports;
+    private Set<BundleStateReport> bundles;
 
     public NodeStateReport(String nodeId, Set<BundleStateReport> bundleStateReports) {
         this.nodeId = nodeId;
-        this.bundleStateReports = bundleStateReports;
+        this.bundles = bundleStateReports;
     }
 
     public String getNodeId() {
@@ -23,11 +25,11 @@ public class NodeStateReport {
         this.nodeId = nodeId;
     }
 
-    public Set<BundleStateReport> getBundleStateReports() {
-        return bundleStateReports;
+    public Set<BundleStateReport> getBundles() {
+        return bundles;
     }
 
-    public void setBundleStateReports(Set<BundleStateReport> bundleStateReports) {
-        this.bundleStateReports = bundleStateReports;
+    public void setBundles(Set<BundleStateReport> bundleStateReports) {
+        this.bundles = bundleStateReports;
     }
 }
