bd97549a05fcece8bfba9f529f834494cd62d222
achaabni <achaabni@jahia.com>
2015-12-07 10:01:07 +0100

    Javadoc update

--- core/src/main/java/org/jahia/modules/modulemanager/payload/OperationState.java
+++ core/src/main/java/org/jahia/modules/modulemanager/payload/OperationState.java
@@ -3,35 +3,44 @@ package org.jahia.modules.modulemanager.payload;
 import java.io.Serializable;
 
 /**
+ * Represents the values of a Node Operation
  * @author achaabni
  */
 public class OperationState implements Serializable{
 
     /**
-     * name
+     * Defines the name.
      */
     private String name;
 
     /**
-     * action
+     * Defines what action was called for this operation.
      */
     private String action;
 
     /**
-     * info
+     * Defines the information saved for the operation.
      */
     private String info;
 
     /**
-     * state
+     * Defines the state of the operation.
      */
     private String state;
 
     /**
-     * completed
+     * Defines if an operation was completed or not.
      */
     private boolean completed;
 
+    /**
+     * Initializes an instance of this class.
+     * @param name name
+     * @param action action
+     * @param info info
+     * @param state state
+     * @param completed <code>true</code> if the operation is closed
+     */
     public OperationState(String name, String action, String info, String state, boolean completed) {
         this.name = name;
         this.action = action;
