1e5b1fb72572b132e5dfaf65bac908a6ac116363
achaabni <achaabni@jahia.com>
2016-06-09 13:39:33 +0200

    BACKLOG-5713

--- core/src/main/bin/stopBundle.sh
+++ core/src/main/bin/stopBundle.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# example script to start a bundle using the module manager's REST API
+# example script to stop a bundle using the module manager's REST API
 DX_DISPLAY_USAGE="Script to stop a bundle using the module manager's REST API.\n
 \nUsage:\n$0 bundleName\n"
 
@@ -7,5 +7,6 @@ DX_DISPLAY_USAGE="Script to stop a bundle using the module manager's REST API.\n
 source ./common.sh
 #start bundle REST call
 echo Stopping bundle $1...
-STOP_OUTPUT=`curl $CURL_OPTIONS --request POST $DX_REST_URL/$1/_stop`
+DATA="target=$2";
+STOP_OUTPUT=`curl $CURL_OPTIONS --data DATA --request POST $DX_REST_URL/$1/_stop`
 echo "Stop result=$STOP_OUTPUT"
