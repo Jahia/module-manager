b166c17ef4d836524306fca387cc57c424f66b30
achaabni <achaabni@jahia.com>
2016-06-09 14:03:13 +0200

    BACKLOG-6043 : REST - command line scripts for module management

--- core/src/main/bin/uninstallBundle.sh
+++ core/src/main/bin/uninstallBundle.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-# example script to start a bundle using the module manager's REST API
+# example script to uninstall a bundle using the module manager's REST API
 DX_DISPLAY_USAGE="Script to uninstall a bundle using the module manager's REST API.\n
 \nUsage:\n$0 bundleName\n"
 
 source ./common.sh
-#undeploy bundle REST call
+#uninstall bundle REST call
 echo Uninstalling bundle $1...
-UNINSTALL_OUTPUT=`curl $CURL_OPTIONS --request POST $DX_REST_URL/$1/_uninstall`
+DATA="target=$2";
+UNINSTALL_OUTPUT=`curl $CURL_OPTIONS --data DATA --request POST $DX_REST_URL/$1/_uninstall`
 echo "Uninstall result=$UNINSTALL_OUTPUT"
