e9786ddd4d2a3cbe0f7064c11a25135e1faabfc1
David Griffon <dgriffon@jahia.com>
2016-06-13 10:18:09 +0200

    BACKLOG-5714 : Protect the API by a specific permission

--- core/src/main/java/org/jahia/modules/modulemanager/rest/ModuleManagerApplicationConfig.java
+++ core/src/main/java/org/jahia/modules/modulemanager/rest/ModuleManagerApplicationConfig.java
@@ -47,6 +47,7 @@ import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+import org.jahia.modules.modulemanager.rest.filters.ModuleManagerAuthenticationRequestFilter;
 
 /**
  * Application configuration component.
@@ -60,6 +61,6 @@ public class ModuleManagerApplicationConfig extends ResourceConfig {
      * Initializes an instance of this class providing a list of classes to be registered.
      */
     public ModuleManagerApplicationConfig() {
-        super(MultiPartFeature.class, ModuleManagerResource.class, JacksonJaxbJsonProvider.class, ModuleManagerExceptionMapper.class);
+        super(MultiPartFeature.class, ModuleManagerResource.class, JacksonJaxbJsonProvider.class, ModuleManagerExceptionMapper.class, ModuleManagerAuthenticationRequestFilter.class);
     }
 }
---
+++ core/src/main/java/org/jahia/modules/modulemanager/rest/filters/ModuleManagerAuthenticationRequestFilter.java
@@ -0,0 +1,136 @@
+/**
+ * ==========================================================================================
+ * =                   JAHIA'S DUAL LICENSING - IMPORTANT INFORMATION                       =
+ * ==========================================================================================
+ *
+ *                                 http://www.jahia.com
+ *
+ *     Copyright (C) 2002-2016 Jahia Solutions Group SA. All rights reserved.
+ *
+ *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
+ *     1/GPL OR 2/JSEL
+ *
+ *     1/ GPL
+ *     ==================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *     2/ JSEL - Commercial and Supported Versions of the program
+ *     ===================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     Alternatively, commercial and supported versions of the program - also known as
+ *     Enterprise Distributions - must be used in accordance with the terms and conditions
+ *     contained in a separate written agreement between you and Jahia Solutions Group SA.
+ *
+ *     If you are unsure which license is appropriate for your use,
+ *     please contact the sales department at sales@jahia.com.
+ */
+package org.jahia.modules.modulemanager.rest.filters;
+
+import org.jahia.exceptions.JahiaUnauthorizedException;
+import org.jahia.services.content.JCRSessionFactory;
+import org.jahia.services.content.JCRSessionWrapper;
+import org.jahia.services.content.decorator.JCRUserNode;
+import org.jahia.services.usermanager.JahiaUser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Priority;
+import javax.jcr.RepositoryException;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Priorities;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.security.Principal;
+
+/**
+ * JAX-RS Filter that filters only users that match the required permission
+ */
+
+@Provider
+@Priority(Priorities.AUTHENTICATION)
+public class ModuleManagerAuthenticationRequestFilter implements ContainerRequestFilter {
+
+    @Context
+    HttpServletRequest httpServletRequest;
+
+    final private static String REQUIRED_PERMISSON = "adminTemplates";
+
+    private static final Logger log = LoggerFactory.getLogger(ModuleManagerAuthenticationRequestFilter.class);
+
+    public ModuleManagerAuthenticationRequestFilter() {
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        try {
+            final JahiaUser user = getAllowedJahiaUser();
+            if (user == null) {
+                requestContext.abortWith(Response
+                        .status(Response.Status.UNAUTHORIZED)
+                        .entity("Unauthorized request")
+                        .build());
+                return;
+            }
+            requestContext.setSecurityContext(new SecurityContext() {
+                @Override
+                public Principal getUserPrincipal() {
+                    return user;
+                }
+
+                @Override
+                public boolean isUserInRole(String role) {
+                    return httpServletRequest.isUserInRole(role);
+                }
+
+                @Override
+                public boolean isSecure() {
+                    return httpServletRequest.isSecure();
+                }
+
+                @Override
+                public String getAuthenticationScheme() {
+                    return httpServletRequest.getScheme();
+                }
+            });
+        } catch (JahiaUnauthorizedException e) {
+
+        }
+    }
+
+    private JahiaUser getAllowedJahiaUser() throws JahiaUnauthorizedException {
+        JCRUserNode user = null;
+        try {
+            JCRSessionWrapper currentUserSession = JCRSessionFactory.getInstance().getCurrentUserSession();
+            user =  currentUserSession.getUserNode();
+            if (currentUserSession.getRootNode().hasPermission(REQUIRED_PERMISSON)) {
+                return user.getJahiaUser();
+            }
+        } catch (Exception e) {
+            log.debug("An error occurs while accessing the API", e);
+        }
+        log.warn("Unauthorized access to the API by user {}", user != null ? user.getPath() : "unknown");
+        throw new JahiaUnauthorizedException("not authorized");
+    }
+}
