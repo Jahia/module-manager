faf0714a5e57f55b47df83c1655e8bc7f4fd9439
Sergiy Shyrkov <sshyrkov@jahia.com>
2016-07-22 15:46:18 +0200

    BACKLOG-6051 - dropped command line scripts for bundle/node state reports as the functionality is not included now

--- core/src/main/bin/getBundleState.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# example script to get a bundle state on a list of nodes using the module manager's REST API
-source ./common.sh
-#get bundle state REST call
-echo Getting state for  bundle $1...
-STATE_OUTPUT=`curl $CURL_OPTIONS --request GET $DX_REST_URL/$1/_state`
-echo "State result=$STATE_OUTPUT"
--- core/src/main/bin/getNodesBundleStates.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# example script to get a list of node report bundles state using the module manager's REST API
-source ./common.sh
-#get nodes bundle state REST call
-STATES_OUTPUT=`curl $CURL_OPTIONS --request GET $DX_REST_URL/_states`
-echo "States result=$STATES_OUTPUT"
