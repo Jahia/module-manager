f714f2888735bdc3824ef76cd86c80f960313297
Alex Karmanov <alexandrk_@hotmail.com>
2022-10-05 10:23:16 -0400

    BACKLOG-17379 Update service to handle deletion of config

--- core/src/main/java/org/jahia/modules/modulemanager/message/MessageServiceImpl.java
+++ core/src/main/java/org/jahia/modules/modulemanager/message/MessageServiceImpl.java
@@ -1,41 +1,35 @@
 package org.jahia.modules.modulemanager.message;
 
+import org.jahia.modules.modulemanager.util.ConfigUtil;
 import org.jahia.services.modulemanager.util.PropertiesManager;
 import org.jahia.services.modulemanager.util.PropertiesValues;
-import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedServiceFactory;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Modified;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.binding.message.Severity;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Service to manage custom messages displayed in module manager
  */
 @Component(
-        service = MessageService.class,
-        immediate = true,
-        name = "org.jahia.modules.modulemanager.message.admin"
+        service = {MessageService.class, ManagedServiceFactory.class},
+        property = "service.pid=org.jahia.modules.modulemanager.message",
+        immediate = true
 )
-public class MessageServiceImpl implements MessageService {
+public class MessageServiceImpl implements MessageService, ManagedServiceFactory {
     private static final Logger logger = LoggerFactory.getLogger(MessageServiceImpl.class);
 
     private static final String CONFIG_KEY = "message";
     private static final List<CustomMessage> messages = new ArrayList<>();
 
-    @Activate
-    public void activate(Map<String, String> props) {
-        logger.debug("Activating message service");
-        clearConstraintsByPid(props.get("service.pid"));
-        parseConfig(props);
-    }
-
-    private void parseConfig(Map<String, String> props) {
+    private void parseConfig(String pid, Dictionary<String, ?> props) {
         logger.debug("Parsing configuration property values");
-        String pid = props.get("service.pid");
-        PropertiesManager pm = new PropertiesManager(props);
+        PropertiesManager pm = new PropertiesManager(ConfigUtil.getMap(props));
         PropertiesValues messageProp = pm.getValues().getValues(CONFIG_KEY);
         for (String key: messageProp.getKeys()) {
             String m = messageProp.getProperty(key);
@@ -46,28 +40,37 @@ public class MessageServiceImpl implements MessageService {
         logger.debug("Configuration parsed");
     }
 
-    @Modified
-    public void modified(Map<String, String> props) {
-        String pid = props.get("service.pid");
-        logger.debug("Updating message configuration {}...", pid);
-        clearConstraintsByPid(pid);
-        parseConfig(props);
-        logger.debug("Message configuration updated.");
-    }
-
     private void clearConstraintsByPid(String pid) {
         if (pid != null) {
-            messages.forEach(m -> {
-                if (m.getPid().equals(pid)) {
-                    messages.remove(m);
-                }
-            });
+            messages.removeAll(messages.stream().filter(m -> m.getPid().equals(pid)).collect(Collectors.toList()));
         }
     }
 
     @Override
     public List<CustomMessage> getAllMessages() {
         return messages;
+    }
+
+    @Override
+    public String getName() {
+        return "Module Manager Message Config";
+    }
+
+    @Override
+    public void updated(String pid, Dictionary<String, ?> dictionary) throws ConfigurationException {
+        if (dictionary == null) {
+            return;
+        }
+
+        logger.debug("Updating message configuration {}...", pid);
+        clearConstraintsByPid(pid);
+        parseConfig(pid, dictionary);
+        logger.debug("Message configuration updated.");
+    }
+
+    @Override
+    public void deleted(String pid) {
+        clearConstraintsByPid(pid);
     }
 
     private class MessageComparator implements Comparator<CustomMessage> {
