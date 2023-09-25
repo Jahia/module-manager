48b9ac775fb90eb4a617256e7c78f1a5b085957e
kevan Jahanshahi <kevan@jahia.com>
2023-09-25 10:58:59 +0200

    TECH-1401: Stop using Shiro classes (#117)
    * TECH-1401: Stop using Shiro classes
    
    * TECH-1401: Stop using Shiro classes (cleanup imports)

--- core/src/main/java/org/jahia/modules/modulemanager/rest/filters/ModuleManagerAuthenticationRequestFilter.java
+++ core/src/main/java/org/jahia/modules/modulemanager/rest/filters/ModuleManagerAuthenticationRequestFilter.java
@@ -21,7 +21,6 @@ import java.security.Principal;
 import javax.annotation.Priority;
 import javax.jcr.RepositoryException;
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.NotAuthorizedException;
 import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
@@ -29,8 +28,6 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.subject.Subject;
 import org.jahia.services.content.JCRSessionFactory;
 import org.jahia.services.content.JCRSessionWrapper;
 import org.jahia.services.usermanager.JahiaUser;
@@ -54,24 +51,13 @@ public class ModuleManagerAuthenticationRequestFilter implements ContainerReques
     @Context
     HttpServletRequest httpServletRequest;
 
-    private Subject getAuthenticatedSubject() {
-        try {
-            return WebUtils.getAuthenticatedSubject(httpServletRequest);
-        } catch (AuthenticationException e) {
-            throw new NotAuthorizedException(e.getMessage(), HttpServletRequest.BASIC_AUTH);
-        }
-    }
-
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
         JahiaUser user = JCRSessionFactory.getInstance().getCurrentUser();
         String username = JahiaUserManagerService.GUEST_USERNAME;
-        if (JahiaUserManagerService.isGuest(user)) {
-            Subject subject = getAuthenticatedSubject();
-            if (subject != null && subject.hasRole(REQUIRED_ROLE)) {
-                // user has the required role: allow access
-                return;
-            }
+        if (JahiaUserManagerService.isGuest(user) && WebUtils.authenticatedSubjectHasRole(httpServletRequest, REQUIRED_ROLE)) {
+            // user has the required role: allow access
+            return;
         } else {
             try {
                 JCRSessionWrapper currentUserSession = JCRSessionFactory.getInstance().getCurrentUserSession();
