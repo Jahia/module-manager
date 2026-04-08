1e698c6767a5a7025e7195dfe3483912c7109cd4
Florent BOURASSE <fbourasse@jahia.com>
2026-04-08 23:15:08 +0200

    test: addition of test with a second forge mockup
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

---
+++ tests/cypress/e2e/03-two-urls.cy.ts
@@ -0,0 +1,34 @@
+import {DocumentNode} from 'graphql'
+import {createSite, publishAndWaitJobEnding} from "@jahia/cypress";
+
+describe('Two Forge URLs', () => {
+
+    let addForgeUrl: DocumentNode
+    addForgeUrl = require('graphql-tag/loader!../fixtures/graphql/mutation/addForgeUrl.graphql')
+    const siteKey = 'mySite'
+
+    before(() => {
+        cy.login();
+        createSite(siteKey, {
+            languages: 'en',
+            templateSet: 'forge-mockup',
+            serverName: 'jahia',
+            locale: 'en',
+        });
+        publishAndWaitJobEnding('/sites/' + siteKey);
+        cy.apollo({
+            mutation: addForgeUrl
+        });
+    })
+
+    it('Check the presence of a mockup module', () => {
+        cy.login();
+        cy.visit('/jahia/administration/manageModules');
+        cy.visit('/cms/adminframe/default/en/settings.manageModules.html');
+        cy.get('#available-modules-tab').click();
+        cy.get('#availableModuleTabs i.material-icons').click();
+        cy.get('#siteSettings input.form-control').click();
+        cy.get('#siteSettings input.form-control').type('mockup');
+        cy.contains('module-mockup').should('be.visible');
+    })
+})
