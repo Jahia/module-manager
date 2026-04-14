31c4095babf09457de6b0914b30b1cc55ec36037
Florent BOURASSE <fbourasse@jahia.com>
2026-04-15 00:09:09 +0200

    feat: some refactor
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

--- core/src/main/java/org/jahia/modules/modulemanager/forge/ForgeConstants.java
+++ core/src/main/java/org/jahia/modules/modulemanager/forge/ForgeConstants.java
@@ -1,4 +1,16 @@
 package org.jahia.modules.modulemanager.forge;
 
 public class ForgeConstants {
+    public static final String JSON_ARRAY_MODULES = "modules";
+    public static final String JSON_ATT_NAME = "name";
+    public static final String JSON_ATT_GROUP_ID = "groupId";
+    public static final String JSON_ARRAY_VERSIONS = "versions";
+    public static final String JSON_ATT_VERSION = "version";
+    public static final String JSON_ATT_REQUIRED_VERSION = "requiredVersion";
+    public static final String JSON_ATT_REMOTE_URL = "remoteUrl";
+    public static final String JSON_ATT_PATH = "path";
+    public static final String JSON_ATT_ICON = "icon";
+    public static final String JSON_ATT_TITLE = "title";
+    public static final String JSON_ATT_DOWNLOAD_URL = "downloadUrl";
+    public static final String MODULE_LIST_JSON_PATH = "/contents/modules-repository.moduleList.json";
 }
--- core/src/main/java/org/jahia/modules/modulemanager/forge/ForgeService.java
+++ core/src/main/java/org/jahia/modules/modulemanager/forge/ForgeService.java
@@ -35,6 +35,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -49,14 +51,14 @@ public class ForgeService {
 
     private static final Logger logger = LoggerFactory.getLogger(ForgeService.class);
     private final List<Module> modules = new ArrayList<Module>();
-    private final  long loadModulesDelay;
+    private final long loadModulesDelay;
     private HttpClientService httpClientService;
     private long lastModulesLoad = new Date().getTime();
     private boolean flushModules = true;
     private ForgeConfigFactory forgeConfigFactory;
 
     public ForgeService() {
-        loadModulesDelay = SettingsBean.getInstance().getLong("jahia.settings.forgeModulesUpdateDelay",86400000L);
+        loadModulesDelay = SettingsBean.getInstance().getLong("jahia.settings.forgeModulesUpdateDelay", 86400000L);
     }
 
     public Collection<ForgeConfig> getForgeConfigs() {
@@ -82,39 +84,39 @@ public class ForgeService {
         if (flushModules || expired) {
             modules.clear();
             for (ForgeConfig forgeConfig : getForgeConfigs()) {
-                final String url = forgeConfig.getUrl() + "/contents/modules-repository.moduleList.json";
+                final String url = forgeConfig.getUrl() + ForgeConstants.MODULE_LIST_JSON_PATH;
                 logger.debug("Start retrieving module list from {}", url);
                 final Map<String, String> headers = new HashMap<String, String>();
                 final String user = forgeConfig.getUser();
                 final String password = forgeConfig.getPassword();
                 if (!StringUtils.isNotEmpty(user) && !StringUtils.isNotEmpty(password)) {
-                    headers.put("Authorization", "Basic " + Base64.encode((user + ":" + password).getBytes(StandardCharsets.UTF_8)));
+                    headers.put(HttpHeaders.AUTHORIZATION, "Basic " + Base64.encode((user + ":" + password).getBytes(StandardCharsets.UTF_8)));
                 }
-                headers.put("accept", "application/json");
+                headers.put(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON);
 
                 final String jsonModuleList = httpClientService.executeGet(url, headers);
                 try {
                     final JSONArray modulesRoot = new JSONArray(jsonModuleList);
-                    final JSONArray moduleList = modulesRoot.getJSONObject(0).getJSONArray("modules");
+                    final JSONArray moduleList = modulesRoot.getJSONObject(0).getJSONArray(ForgeConstants.JSON_ARRAY_MODULES);
                     for (int i = 0; i < moduleList.length(); i++) {
                         boolean add = true;
 
                         final JSONObject moduleObject = moduleList.getJSONObject(i);
                         for (Module m : modules) {
-                            if (StringUtils.equals(m.getId(), moduleObject.getString("name")) && StringUtils.equals(m.getGroupId(), moduleObject.getString("groupId"))) {
+                            if (StringUtils.equals(m.getId(), moduleObject.getString(ForgeConstants.JSON_ATT_NAME)) && StringUtils.equals(m.getGroupId(), moduleObject.getString(ForgeConstants.JSON_ATT_GROUP_ID))) {
                                 add = false;
                                 break;
                             }
                         }
                         if (add) {
-                            final JSONArray moduleVersions = moduleObject.getJSONArray("versions");
+                            final JSONArray moduleVersions = moduleObject.getJSONArray(ForgeConstants.JSON_ARRAY_VERSIONS);
                             final SortedMap<Version, JSONObject> sortedVersions = new TreeMap<Version, JSONObject>();
                             final Version jahiaVersion = new Version(Jahia.VERSION);
 
                             for (int j = 0; j < moduleVersions.length(); j++) {
                                 final JSONObject object = moduleVersions.getJSONObject(j);
-                                final Version version = new Version(object.getString("version"));
-                                final Version requiredVersion = new Version(StringUtils.substringAfter(object.getString("requiredVersion"), "version-"));
+                                final Version version = new Version(object.getString(ForgeConstants.JSON_ATT_VERSION));
+                                final Version requiredVersion = new Version(StringUtils.substringAfter(object.getString(ForgeConstants.JSON_ATT_REQUIRED_VERSION), "version-"));
                                 if (requiredVersion.compareTo(jahiaVersion) <= 0 && requiredVersion.getMajorVersion() == jahiaVersion.getMajorVersion()) {
                                     sortedVersions.put(version, object);
                                 }
@@ -122,16 +124,16 @@ public class ForgeService {
                             if (!sortedVersions.isEmpty()) {
                                 final Module module = new Module();
                                 final JSONObject versionObject = sortedVersions.get(sortedVersions.lastKey());
-                                module.setRemoteUrl(moduleObject.getString("remoteUrl"));
-                                module.setRemotePath(moduleObject.getString("path"));
-                                if (moduleObject.has("icon")) {
-                                    module.setIcon(moduleObject.getString("icon"));
+                                module.setRemoteUrl(moduleObject.getString(ForgeConstants.JSON_ATT_REMOTE_URL));
+                                module.setRemotePath(moduleObject.getString(ForgeConstants.JSON_ATT_PATH));
+                                if (moduleObject.has(ForgeConstants.JSON_ATT_ICON)) {
+                                    module.setIcon(moduleObject.getString(ForgeConstants.JSON_ATT_ICON));
                                 }
-                                module.setVersion(versionObject.getString("version"));
-                                module.setName(moduleObject.getString("title"));
-                                module.setId(moduleObject.getString("name"));
-                                module.setGroupId(moduleObject.getString("groupId"));
-                                module.setDownloadUrl(versionObject.getString("downloadUrl"));
+                                module.setVersion(versionObject.getString(ForgeConstants.JSON_ATT_VERSION));
+                                module.setName(moduleObject.getString(ForgeConstants.JSON_ATT_TITLE));
+                                module.setId(moduleObject.getString(ForgeConstants.JSON_ATT_NAME));
+                                module.setGroupId(moduleObject.getString(ForgeConstants.JSON_ATT_GROUP_ID));
+                                module.setDownloadUrl(versionObject.getString(ForgeConstants.JSON_ATT_DOWNLOAD_URL));
                                 module.setForgeId(forgeConfig.getPid());
                                 modules.add(module);
                             }
@@ -167,7 +169,7 @@ public class ForgeService {
                 final String user = forgeConfig.getUser();
                 final String password = forgeConfig.getPassword();
                 if (!StringUtils.isNotEmpty(user) && !StringUtils.isNotEmpty(password)) {
-                    httpMethod.addHeader("Authorization", "Basic " + Base64.encode((user + ":" + password).getBytes(StandardCharsets.UTF_8)));
+                    httpMethod.addHeader(HttpHeaders.AUTHORIZATION, "Basic " + Base64.encode((user + ":" + password).getBytes(StandardCharsets.UTF_8)));
                 }
                 final CloseableHttpClient httpClient = httpClientService.getHttpClient(url);
                 try (CloseableHttpResponse httpResponse = httpClient.execute(httpMethod)) {
