c66305e5d51b7e75e7d424654f788cba22c7910b
Florent BOURASSE <fbourasse@jahia.com>
2026-04-15 00:25:23 +0200

    feat: addition of test to check the registry after migration
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

--- tests/cypress/e2e/01-patch-check.cy.ts
+++ tests/cypress/e2e/01-patch-check.cy.ts
@@ -2,7 +2,9 @@ import {DocumentNode} from 'graphql'
 
 describe('Patch check', () => {
     let getForgeUrl: DocumentNode
+    let getNodeTypeByName: DocumentNode
     getForgeUrl = require('graphql-tag/loader!../fixtures/graphql/query/getForgeUrl.graphql')
+    getNodeTypeByName = require('graphql-tag/loader!../fixtures/graphql/query/getNodeTypeByName.graphql')
 
     it('Check the migration of the forge settings to configuration files', () => {
         cy.login();
@@ -22,5 +24,36 @@ describe('Patch check', () => {
         }).should((response: any) => {
             expect(response.data.admin.jahia.configuration.value).to.equal('https://store.jahia.org/en/sites/private-app-store');
         })
+    })
+
+    it('Check that the legacy nodetypes are not present anymore', () => {
+        cy.login();
+        cy.apollo({
+            errorPolicy: 'all',
+            query: getNodeTypeByName,
+            variables: {
+                "name": "jnt:forgeServerSettings"
+            }
+        }).should(response => {
+            expect(response.errors[0].message).to.contain('javax.jcr.nodetype.NoSuchNodeTypeException: Unknown type : jnt:forgeServerSettings');
+        });
+        cy.apollo({
+            errorPolicy: 'all',
+            query: getNodeTypeByName,
+            variables: {
+                "name": "jnt:forgesServerSettings"
+            }
+        }).should(response => {
+            expect(response.errors[0].message).to.contain('javax.jcr.nodetype.NoSuchNodeTypeException: Unknown type : jnt:forgesServerSettings');
+        });
+        cy.apollo({
+            errorPolicy: 'all',
+            query: getNodeTypeByName,
+            variables: {
+                "name": "jnt:serverSettingsManageForges"
+            }
+        }).should(response => {
+            expect(response.errors[0].message).to.contain('javax.jcr.nodetype.NoSuchNodeTypeException: Unknown type : jnt:serverSettingsManageForges');
+        });
     })
 })
