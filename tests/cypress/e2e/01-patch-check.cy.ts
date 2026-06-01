4c0e122981b3b939c3ac11d949161b28d9e73111
Florent BOURASSE <fbourasse@jahia.com>
2026-06-01 22:57:40 +0200

    fix: removal of test following non removal of node types
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

--- tests/cypress/e2e/01-patch-check.cy.ts
+++ tests/cypress/e2e/01-patch-check.cy.ts
@@ -32,35 +32,4 @@ describe('Patch check', () => {
             }
         });
     });
-
-    it('Check that the legacy nodetypes are not present anymore', () => {
-        cy.login();
-        cy.apollo({
-            errorPolicy: 'all',
-            query: getNodeTypeByName,
-            variables: {
-                name: 'jnt:forgeServerSettings'
-            }
-        }).should(response => {
-            expect(response.errors[0].message).to.contain('javax.jcr.nodetype.NoSuchNodeTypeException: Unknown type : jnt:forgeServerSettings');
-        });
-        cy.apollo({
-            errorPolicy: 'all',
-            query: getNodeTypeByName,
-            variables: {
-                name: 'jnt:forgesServerSettings'
-            }
-        }).should(response => {
-            expect(response.errors[0].message).to.contain('javax.jcr.nodetype.NoSuchNodeTypeException: Unknown type : jnt:forgesServerSettings');
-        });
-        cy.apollo({
-            errorPolicy: 'all',
-            query: getNodeTypeByName,
-            variables: {
-                name: 'jnt:serverSettingsManageForges'
-            }
-        }).should(response => {
-            expect(response.errors[0].message).to.contain('javax.jcr.nodetype.NoSuchNodeTypeException: Unknown type : jnt:serverSettingsManageForges');
-        });
-    });
 });
