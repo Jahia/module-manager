091212e91bd49946c0e1fc305223b56c61ed4371
Florent BOURASSE <fbourasse@jahia.com>
2026-06-02 09:03:55 +0200

    feat: modify test so forge mockup is private
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

--- tests/cypress/e2e/04-two-urls.cy.ts
+++ tests/cypress/e2e/04-two-urls.cy.ts
@@ -1,10 +1,14 @@
 import {DocumentNode} from 'graphql';
-import {createSite, publishAndWaitJobEnding} from '@jahia/cypress';
+import {createSite, publishAndWaitJobEnding, deleteSite} from '@jahia/cypress';
 
-describe('Two Forge URLs', () => {
+describe('Two Forge URLs with one private', () => {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const addForgeUrl: DocumentNode = require('graphql-tag/loader!../fixtures/graphql/mutation/addForgeUrl.graphql');
+    const revokeUser: DocumentNode = require('graphql-tag/loader!../fixtures/graphql/mutation/revokeUser.graphql');
     const siteKey = 'mySite';
+    const sitePath = `/sites/` + siteKey;
+    const principal = "u:guest";
+
 
     before(() => {
         cy.login();
@@ -14,7 +18,16 @@ describe('Two Forge URLs', () => {
             serverName: 'jahia',
             locale: 'en'
         });
-        publishAndWaitJobEnding('/sites/' + siteKey);
+
+        cy.apollo({
+            mutation: revokeUser,
+            variables: {
+                pathOrId: sitePath + '/j:acl',
+                name: `DENY_${principal.replace(':', '_')}`,
+                principal: principal
+            }
+        });
+        publishAndWaitJobEnding(sitePath);
         cy.apollo({
             mutation: addForgeUrl,
             variables: {
@@ -24,7 +37,24 @@ describe('Two Forge URLs', () => {
         });
     });
 
+    after(() => {
+        cy.login();
+        deleteSite(siteKey);
+    });
+
     it('Check the presence of a mockup module', () => {
+        // Check that the site holding the forge mockup is private
+        cy.logout();
+        cy.request({
+            url: `http://jahia:8080`,
+            failOnStatusCode: false
+        }).its('status').should('eq', 401)
+        // Check that the json file is not seen as quest
+        cy.request({
+            url: `http://jahia:8080/contents/modules-repository.moduleList.json`,
+            failOnStatusCode: false
+        }).its('status').should('eq', 404)
+
         cy.login();
         cy.visit('/jahia/administration/manageModules');
         cy.visit('/cms/adminframe/default/en/settings.manageModules.html');
